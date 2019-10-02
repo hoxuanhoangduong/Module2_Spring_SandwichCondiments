e860cecb553751058fa05d31650e35ade0b94950
Ho Duong <hxhduong4394@gmail.com>
2019-10-02 19:25:00 +0700

    Add controller

---
+++ src/main/java/com/codegym/controller/CondimentsController.java
@@ -0,0 +1,13 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CondimentsController {
+    @RequestMapping("/save")
+    public String save(@RequestParam("condiment") String[] condiment) {
+        return "sandwich"
+    }
+}
